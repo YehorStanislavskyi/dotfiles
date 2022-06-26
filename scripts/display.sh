9b9ca6122d080f73bc3058646da091f35816d457
YehorStanislavskyi <36374838+YehorStanislavskyi@users.noreply.github.com>
2022-06-26 22:29:33 +0300

    Add files via upload

--- scripts/display.sh
+++ scripts/display.sh
@@ -1,10 +1,10 @@
 #!/bin/bash 
-choices="laptop\ndual\nexternal\nclone" 
-chosen=$(echo -e $choices |rofi -dmenu -i)  
+choices="Laptop\nDual\nExternal\nClone" 
+chosen=$(echo -e $choices |rofi -dmenu -i -font "Iosevka Nerd 16" -hide-scrollbar -no-fixed-num-lines) 
 case "$chosen" in
-    external) xrandr --output eDP1 --off --output DP3 --auto --primary ;;
-    laptop) xrandr --output eDP1 --auto --primary --output DP3 --off ;;
-    clone) xrandr --output eDP1 --auto --output DP3 --auto --same-as  eDP1;;
-    dual) xrandr --output eDP1 --auto --output DP3 --auto --right-of eDP1 --primary ;;
+    External) xrandr --output eDP1 --off --output DP3 --auto --primary ;;
+    Laptop) xrandr --output eDP1 --auto --primary --output DP3 --off ;;
+    Clone) xrandr --output eDP1 --auto --output DP3 --auto --same-as  eDP1;;
+    Dual) xrandr --output eDP1 --auto --output DP3 --auto --right-of eDP1 --primary ;;
 
 esac
