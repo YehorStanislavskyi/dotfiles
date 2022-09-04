ca4e2f529bfb7a72e754a3021657ce8d0135ef9a
YehorStanislavskyi <36374838+YehorStanislavskyi@users.noreply.github.com>
2022-09-04 22:26:09 +0300

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -7,7 +7,6 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 # Launch
-polybar main &
-polybar no-main;
+polybar --config=~/.config/polybar/config.ini main ;
 
 echo "Bar launched..."
